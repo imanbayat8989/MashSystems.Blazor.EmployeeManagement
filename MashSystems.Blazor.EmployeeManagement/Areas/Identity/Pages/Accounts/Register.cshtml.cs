1bb67851df8ac8b4fd27ed7f71d28f66acaa6001
imanbayat8989 <imanbayat8989@gmail.com>
2026-08-16 20:30:30 +0330

    Create the Login page

---
+++ MashSystems.Blazor.EmployeeManagement/Areas/Identity/Pages/Accounts/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MashSystems.Blazor.EmployeeManagement.Areas.Identity.Pages.Accounts
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
