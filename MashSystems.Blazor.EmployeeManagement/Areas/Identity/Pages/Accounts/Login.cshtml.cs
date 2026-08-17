fe2126fe27ea4815b77c6b2f81bc96342f958708
imanbayat8989 <imanbayat8989@gmail.com>
2026-08-17 20:21:24 +0330

    Create login and register page

--- MashSystems.Blazor.EmployeeManagement/Areas/Identity/Pages/Accounts/Login.cshtml.cs
+++ MashSystems.Blazor.EmployeeManagement/Areas/Identity/Pages/Accounts/Login.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace MashSystems.Blazor.EmployeeManagement.Areas.Identity.Pages.Accounts
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public InputModel Input { get; set; }
         public void OnGet()
         {
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return Page();
+        }
+    }
+
+    public class InputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
--- MashSystems.Blazor.EmployeeManagement/Areas/Identity/Pages/Accounts/Register.cshtml.cs
+++ MashSystems.Blazor.EmployeeManagement/Areas/Identity/Pages/Accounts/Register.cshtml.cs
@@ -5,6 +5,8 @@ namespace MashSystems.Blazor.EmployeeManagement.Areas.Identity.Pages.Accounts
 {
     public class RegisterModel : PageModel
     {
+        [BindProperty]
+        public InputModel Input { get; set; }
         public void OnGet()
         {
         }
