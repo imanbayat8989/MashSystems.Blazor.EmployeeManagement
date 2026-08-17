67142056233e7fabad5e5bee9884aab49be3e84b
imanbayat8989 <imanbayat8989@gmail.com>
2026-08-18 02:45:49 +0330

    update Identity

--- MashSystems.Blazor.EmployeeManagement/Program.cs
+++ MashSystems.Blazor.EmployeeManagement/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MashSystems.Blazor.EmployeeManagement.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +10,33 @@ builder.Services.AddServerSideBlazor();
 
 var connectionstring = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// THIS WAS MISSING: Register DataContext with SQL Server
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(connectionstring));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.SignIn.RequireConfirmedEmail = false;
+})
+    .AddEntityFrameworkStores<DataContext>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbcontext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    dbcontext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,6 +45,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
