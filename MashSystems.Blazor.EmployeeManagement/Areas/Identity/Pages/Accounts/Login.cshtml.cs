e7ca8d1e621bf392e7496174ef01fd69a29ad846
imanbayat8989 <imanbayat8989@gmail.com>
2026-08-20 01:39:13 +0330

    create login and logout

--- MashSystems.Blazor.EmployeeManagement/Areas/Identity/Pages/Accounts/Login.cshtml.cs
+++ MashSystems.Blazor.EmployeeManagement/Areas/Identity/Pages/Accounts/Login.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
@@ -6,6 +7,15 @@ namespace MashSystems.Blazor.EmployeeManagement.Areas.Identity.Pages.Accounts
 {
     public class LoginModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+      
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            
+        }
+
         [BindProperty]
         public InputModel Input { get; set; }
         public void OnGet()
@@ -14,6 +24,15 @@ namespace MashSystems.Blazor.EmployeeManagement.Areas.Identity.Pages.Accounts
 
         public async Task<IActionResult> OnPostAsync()
         {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password,
+                    isPersistent: false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return LocalRedirect("~/");
+                }
+            }
             return Page();
         }
     }
