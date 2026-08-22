d4657ee11273bcc33e51440032d2515d4f2362f2
imanbayat8989 <imanbayat8989@gmail.com>
2026-08-22 17:53:35 +0330

    Create Employee Form

---
+++ MashSystems.Blazor.EmployeeManagement/Models/DTOs/AddEmployeeForm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MashSystems.Blazor.EmployeeManagement.Models.DTOs
+{
+    public class AddEmployeeForm
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ImgUrl { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+        [Required]
+        public EmployeeType Type { get; set; }
+        [Required]
+        public Position Position { get; set; }
+    }
+}
--- MashSystems.Blazor.EmployeeManagement/Services/EmployeeService.cs
+++ MashSystems.Blazor.EmployeeManagement/Services/EmployeeService.cs
@@ -1,4 +1,6 @@
 ﻿using MashSystems.Blazor.EmployeeManagement.Data;
+using MashSystems.Blazor.EmployeeManagement.Models;
+using MashSystems.Blazor.EmployeeManagement.Models.DTOs;
 using MashSystems.Blazor.EmployeeManagement.Models.Responses;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +9,7 @@ namespace MashSystems.Blazor.EmployeeManagement.Services
     public interface IEmployeeService
     {
         Task<GetEmployeesResponse> GetEmployees();
+        Task<BaseResponse> AddEmployee(AddEmployeeForm form);
     }
     public class EmployeeService : IEmployeeService
     {
@@ -15,6 +18,44 @@ namespace MashSystems.Blazor.EmployeeManagement.Services
         public EmployeeService(IDbContextFactory<DataContext> factory)
         {
             _factory = factory;
+        }
+
+        public async Task<BaseResponse> AddEmployee(AddEmployeeForm form)
+        {
+            var response = new BaseResponse();
+            try
+            {
+                using (var context = _factory.CreateDbContext())
+                {
+                    context.Add(new Employee
+                    {
+                        Name = form.Name,
+                        Position = form.Position,
+                        Salary = form.Salary,
+                        Type = form.Type,
+                        ImgUrl = form.ImgUrl,
+                    });
+                    var result = await context.SaveChangesAsync();
+
+                    if (result == 1)
+                    {
+                        response.StatusCode = 200;
+                        response.Message = "Employee Added Succesfully ";
+                    }
+                    else
+                    {
+                        response.StatusCode = 400;
+                        response.Message = "Error occured while adding employee ";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 500;
+                response.Message ="Error adding employee" + ex.Message;
+            }
+            return response;
+
         }
 
         public async Task<GetEmployeesResponse> GetEmployees()
